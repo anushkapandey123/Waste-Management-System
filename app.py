c4867bfee1e1446adf18d8991a057efb5c0fdde0
reachdevikas <79503043+reachdevikas@users.noreply.github.com>
2023-04-17 23:56:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from PIL import Image
 
 main_image = Image.open('main_banner.png')
 st.image(main_image,use_column_width='always')
-st.title("Team 7 - PES1UG20CS072, PES1UG20CS073, PES1UG20CS106, PES1UG20CS123")
+st.title("Team 6 - PES1UG20CS072, PES1UG20CS073, PES1UG20CS106, PES1UG20CS123")
 st.subheader("Team 6 - Anushka Pandey, Anushka Siri Raghunandan, B Praneetha, Devika S")
 def main():
     st.title("Project 3")
     
-    st.subheader("WASTE MANAGEMENT SYSTEM - UE20CS301")
+    st.subheader("WASTE MANAGEMENT SYSTEM - UE20CS321")
 
     main1_image = Image.open('Screenshot (11075).png')
     st.image(main1_image,use_column_width='always')
