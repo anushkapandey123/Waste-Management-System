600f83c83084451650131e94e4570d68fa36b3f6
reachdevikas <79503043+reachdevikas@users.noreply.github.com>
2023-04-18 22:45:50 +0530

    Update 06_AreaHasATruck.py

--- pages/06_AreaHasATruck.py
+++ pages/06_AreaHasATruck.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 import database as db
 import pandas as pd
-st.title("PES1UG20CS123")
+st.title("PES1UG20CS072, PES1UG20CS073, PES1UG20CS106, PES1UG20CS123")
 st.subheader("Area Has A Truck Details")
 
 
